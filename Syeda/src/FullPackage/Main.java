2a2d9acabf420141c34f1016eb5f4a128141479e
Areeb <areeb_sheikh96@yahoo.com>
2022-03-01 19:04:10 -0500

    added file

---
+++ Syeda/src/FullPackage/Main.java
@@ -0,0 +1,11 @@
+package FullPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Method x=new Method();
+			x.syeda();
+			x.areeb();
+	}
+}
---
+++ Syeda/src/FullPackage/Method.java
@@ -0,0 +1,15 @@
+package FullPackage;
+
+public class Method {
+
+	
+	public void syeda() {
+		System.out.print("I Love Syeda");
+	}
+	
+	public void areeb() {
+			System.out.println("i love areeb");
+	}
+	
+
+}
